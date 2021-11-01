1d37c0e20048a2f3a7e2af1a8a79be3070b3edfd
Tatarize <tatarize@gmail.com>
2021-11-01 14:10:56 -0700

    Correct setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup
 setup(
     install_requires=[
-        "meerk40t>=0.7.0-post40",
+        "wxPython",
     ],
-    extras_require={
-        'cv': ["opencv-python-headless>=3.4.0.0"],
-        'cvhead': ["opencv-python>=3.4.0.0"],
-    }
 )
